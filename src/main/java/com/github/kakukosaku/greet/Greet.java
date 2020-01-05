a9d1958f4e7480c905fa6b67f05aa90c312f36dc
guojiangshuai <guojiangshuai@itiger.com>
2020-01-05 22:13:17 +0800

    [add] Greet class

---
+++ src/main/java/com/github/kakukosaku/greet/Greet.java
@@ -0,0 +1,14 @@
+package com.github.kakukosaku.greet;
+
+public class Greet {
+
+    /**
+     * Just Say hello~
+     *
+     * @param args command line args separate by white space.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello 世界, from Java");
+    }
+
+}
