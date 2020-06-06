6effc8f1a131318ae5e353d5d9a941ac9863f0c9
kaku <scugjs@gmail.com>
2020-06-07 07:41:52 +0800

    [add] no xml bean config example

---
+++ src/main/java/com/github/kakukosaku/spring/noxml/AppConfig.java
@@ -0,0 +1,42 @@
+package com.github.kakukosaku.spring.noxml;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Description
+ *
+ * @author kaku
+ * Date    2020/6/7
+ */
+@Configuration
+public class AppConfig {
+
+    @Bean
+    Hello getGreetTool() {
+        return new Hello("kaku");
+    }
+
+    public static void main(String[] args) {
+        ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfig.class);
+        Hello hi = (Hello) ctx.getBean("getGreetTool");
+        hi.greet();
+    }
+
+}
+
+
+class Hello {
+
+    String name;
+
+    Hello(String name) {
+        this.name = name;
+    }
+
+    public void greet() {
+        System.out.println("Hi, " + name + " from bean...");
+    }
+}
