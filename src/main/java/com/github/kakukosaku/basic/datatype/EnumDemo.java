d5489386bc0814c5432f95192737be3aa16257b1
kaku <scugjs@gmail.com>
2020-06-26 09:22:34 +0800

    [add] enum example codes

--- src/main/java/com/github/kakukosaku/basic/datatype/EnumDemo.java
+++ src/main/java/com/github/kakukosaku/basic/datatype/EnumDemo.java
@@ -6,5 +6,47 @@ package com.github.kakukosaku.basic.datatype;
  * @author kaku
  * Date    2020/6/25
  */
-public class EnumDemo {
+public enum EnumDemo {
+    Phone("手机") {
+        @Override
+        String getSize() {
+            return "small";
+        }
+    },
+    Pad("平板") {
+        @Override
+        String getSize() {
+            return "medium";
+        }
+    },
+    Computer("电脑") {
+        @Override
+        String getSize() {
+            return "big";
+        }
+    },
+    Television("电视") {
+        @Override
+        String getSize() {
+            return "big";
+        }
+    };
+
+    private final String translation;
+
+    EnumDemo(String translation) {
+        this.translation = translation;
+    }
+
+    abstract String getSize();
+
+    public String getTranslation() {
+        return this.translation;
+    }
+
+    public static void main(String[] args) {
+        for (EnumDemo e : EnumDemo.values()) {
+            System.out.println(e + " " + e.getTranslation());
+        }
+    }
 }
